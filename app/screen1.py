15e23a4e57ee78725cea335046e8d926aa0ea278
Valentina <vschotborgh02@gmail.com>
2023-04-03 19:07:41 -0500

    Funciones para manejo de la matriz y registros
    En screen1 se colocaron las funciones necesarias para crear la matriz que Jesus usará en JS y para agregar y eliminar records (creoo que quedó pendiente una de las funciones, ahí la puse en los comentarios). En la screen2 pues el cálculo de los datos estadisticos que pedía la profe pero a las últimas dos listas se le cálculo solo la moda :)

--- app/screen1.py
+++ app/screen1.py
@@ -1,5 +1,51 @@
 #getTable(start: int, end: int): int[][] -> obtener matriz que representa la tabla principal
 #deleteRecord(orden: int): void -> eliminar registro
 #addRecord(aquí irán las columnas que decidan que van a conformar la tabla principal): void -> Agregar registro
+#Pendiente: 3.	Imprimir la información de un dato específico solicitado por el usuario (con la información relacionada de las listas)
 
 #Recomiendo haber creado ya la tabla con las nuevas columnas, etc, etc, antes de comenzar a crear las funciones
+import pandas as pd
+import numpy as np
+import eel
+df=pd.read_csv('https://www.datos.gov.co/api/views/7cci-nqqb/rows.csv?accessType=DOWNLOAD')
+#Variables independientes
+Peaton= df['PEATON'].to_list()
+Automovil=df['AUTOMOVIL'].to_list()
+Campaero=df['CAMPAERO'].to_list()
+Camioneta=df['CAMIONETA'].to_list()
+Micro=df['MICRO'].to_list()
+Buseta=df['BUSETA'].to_list()
+Bus=df['BUS'].to_list()
+Camion=df['CAMION'].to_list()
+Volqueta=df['VOLQUETA'].to_list()
+Moto=df['MOTO'].to_list()
+Bicicleta=df['BICICLETA'].to_list()
+Diurnio_Nocturno=df['DIURNIO/NOCTURNO'].to_list()
+#Variables dependientes
+Gravedad=df['GRAVEDAD'].to_list()
+
+#Función que crea matriz que representa la tabla principal
+@eel.expose
+def gettable(peaton, automovil, campaero, camioneta, micro, buseta, bus, camion, volqueta, moto, bicicleta, diurnio_nocturno, gravedad):
+    table=np.array(list(zip(peaton, automovil, campaero, camioneta, micro, buseta,bus, camion,volqueta, moto,bicicleta, diurnio_nocturno,gravedad)))
+    return table
+
+#tabla= gettable(Peaton,Automovil, Campaero,Camioneta,Micro,Buseta,Bus,Camion,Volqueta,Moto,Bicicleta,Diurnio_Nocturno,Gravedad)
+#print(tabla)
+#Eliminar registro
+@eel.expose
+def deleteRecord(datos, f):
+    newtable=np.delete(datos,f, axis=0)
+    return newtable
+
+
+#eliminar=deleteRecord(tabla,0)
+
+#Añadir registro
+@eel.expose
+def addRecord(datos, newrecord):
+    newtable=np.vstack((datos,newrecord))
+    return newtable 
+
+#new=addRecord(tabla,[1,1,1,1,1,1,1,1,1,1,1,'Diurno','Con Heridos'])
+#print(new)
--- app/screen2.py
+++ app/screen2.py
@@ -6,3 +6,105 @@
 
 #-------------------------------------------
 #las gráficas si me toca a mi del lado del cliente porque ahí es donde se grafica xd
+
+
+#Moda, media, mediana y valores máximos y mínimos de las listas usadas
+import statistics
+import eel
+import screen1 
+
+@eel.expose
+def moda(lista):
+    mode=statistics.mode(lista)
+    return mode
+@eel.expose
+def mediana(lista):
+    median=statistics.median(lista)
+    return median
+@eel.expose
+def media(lista):
+    mean=statistics.mean(lista)
+    return mean
+@eel.expose
+def maximo(lista):
+    maximum=max(lista)
+    return maximum
+@eel.expose
+def minimo(lista):
+    minimum=min(lista)
+    return minimum
+
+
+modapeaton=moda(screen1.Peaton)
+medianapeaton=mediana(screen1.Peaton)
+mediapeaton=media(screen1.Peaton)
+maxpeaton=maximo(screen1.Peaton)
+minpeaton=minimo(screen1.Peaton)
+
+modaautomovil=moda(screen1.Automovil)
+medianaautomovil=mediana(screen1.Automovil)
+mediaautomovil=media(screen1.Automovil)
+maxautomovil=maximo(screen1.Automovil)
+minautomovil=minimo(screen1.Automovil)
+
+
+modacampaero=moda(screen1.Campaero)
+medianacampaero=mediana(screen1.Campaero)
+mediacampaero=media(screen1.Campaero)
+maxcampaero=maximo(screen1.Campaero)
+mincampaero=minimo(screen1.Campaero)
+
+modacamioneta=moda(screen1.Camioneta)
+medianacamioneta=mediana(screen1.Camioneta)
+mediacamioneta=media(screen1.Camioneta)
+maxcamioneta=maximo(screen1.Camioneta)
+mincamioneta=minimo(screen1.Camioneta)
+
+modamicro=moda(screen1.Micro)
+medianamicro=mediana(screen1.Micro)
+mediamicro=media(screen1.Micro)
+maxmicro=maximo(screen1.Micro)
+minmicro=minimo(screen1.Micro)
+
+modabuseta=moda(screen1.Buseta)
+medianabuseta=mediana(screen1.Buseta)
+mediabuseta=media(screen1.Buseta)
+maxbuseta=maximo(screen1.Buseta)
+minabuseta=minimo(screen1.Buseta)
+
+modabus=moda(screen1.Bus)
+medianbus=mediana(screen1.Bus)
+mediabus=media(screen1.Bus)
+maxbus=maximo(screen1.Bus)
+minbus=minimo(screen1.Bus)
+
+modacamion=moda(screen1.Camion)
+medianacamion=mediana(screen1.Camion)
+mediacamion=media(screen1.Camion)
+maxcamion=maximo(screen1.Camion)
+mincamion=minimo(screen1.Camion)
+
+modavolqueta=moda(screen1.Volqueta)
+medianvolqueta=mediana(screen1.Volqueta)
+mediavolqueta=media(screen1.Volqueta)
+maxvolqueta=maximo(screen1.Volqueta)
+minvolqueta=minimo(screen1.Volqueta)
+
+modamoto=moda(screen1.Moto)
+medianamoto=mediana(screen1.Moto)
+mediamoto=media(screen1.Moto)
+maxmoto=maximo(screen1.Moto)
+minmoto=minimo(screen1.Moto)
+
+modabicicleta=moda(screen1.Bicicleta)
+medianabicicleta=mediana(screen1.Bicicleta)
+mediabicicleta=media(screen1.Bicicleta)
+maxbicicleta=maximo(screen1.Bicicleta)
+minabicicleta=minimo(screen1.Bicicleta)
+
+modadiurnio_nocturno=moda(screen1.Diurnio_Nocturno)
+modagravedad=moda(screen1.Gravedad)
+
+#BTW solo se calcula la moda de las listas de Diurnio/Nocturno y gravedad porque son listas que no contienen
+#datos númericos para hacer los otros cálculos como media, mediana, etc.
+
